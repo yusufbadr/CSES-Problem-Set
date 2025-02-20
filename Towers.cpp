5b80dc07df77c68fd2bf139996bc6e6c1a615a97
Yusuf Badr <102367494+yusufbadr@users.noreply.github.com>
2025-02-20 17:13:52 +0200

    Create Towers.cpp

---
+++ Towers.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define ceil(a, b) ((a+b-1)/b)
+#define mod ((int) 1e9+7)
+#define INF LLONG_MAX
+#define sq(x) ((x)*(x))
+#define sz(a) (int) a.size()
+#define PI (2*acos(0.0)) // M_PI
+
+void YusufBadr(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+//int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//int dy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+
+
+void solve(){
+    int n; cin >> n;
+
+    multiset<int> st;
+
+    for (int i = 0; i < n; ++i) {
+        int t; cin >> t;
+        auto it = st.upper_bound(t);
+        if (it!=st.end())
+            st.erase(it);
+        st.insert(t);
+
+    }
+
+    cout << st.size() << endl;
+    
+}
+
+
+signed main() {
+    YusufBadr();
+    int t = 1;
+//    cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
