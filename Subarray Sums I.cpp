66ca27f48c2c2171b48cfbd5b9929a5c1e2b6334
Yusuf Badr <102367494+yusufbadr@users.noreply.github.com>
2025-02-22 14:31:32 +0200

    Create Subarray Sums I.cpp

---
+++ Subarray Sums I.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define ceil(a, b) ((a+b-1)/b)
+#define mod ((int) 1e9+7)
+#define INF LLONG_MAX
+#define sq(x) ((x)*(x))
+#define sz(a) (int) a.size()
+#define PI (2*acos(0.0)) // M_PI
+
+void YusufBadr(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+//int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//int dy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+
+
+void solve(){
+    int n, x;
+    cin >> n >> x;
+    vector<int> v(n);
+    for (auto&i:v) cin >> i;
+
+    int l=0, r=0, sum=0, cnt=0;
+    while (r<n){
+        while (r<n && sum<x){
+            sum+=v[r++];
+        }
+
+        while (l<=r && sum>x){
+            sum-=v[l++];
+        }
+
+        if (sum==x) {
+            cnt++;
+            sum-=v[l++];
+        }
+    }
+
+    cout << cnt << endl;
+
+
+}
+
+
+signed main() {
+    YusufBadr();
+    int t = 1;
+//    cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
