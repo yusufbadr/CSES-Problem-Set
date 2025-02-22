f5a5c318f7ea9e22f5327b31c38177b2f01ba831
Yusuf Badr <102367494+yusufbadr@users.noreply.github.com>
2025-02-22 16:48:12 +0200

    Create Subarray Sums II.cpp

---
+++ Subarray Sums II.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define ceil(a, b) ((a+b-1)/b)
+#define mod ((int) 1e9+7)
+#define INF LLONG_MAX
+#define sq(x) ((x)*(x))
+#define sz(a) (int) a.size()
+#define PI (2*acos(0.0)) // M_PI
+
+void YusufBadr(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+//int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//int dy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+
+
+void solve(){
+    int n, x; cin >> n >> x;
+    map<int, int> mp;
+    mp[0]=1;
+    int currSum = 0, res=0;
+    for (int i = 0; i < n; ++i) {
+        int t; cin >> t;
+        currSum+=t;
+        res+=mp[currSum - x];
+        mp[currSum]++;
+    }
+
+    cout << res << endl;
+    
+}
+
+
+signed main() {
+    YusufBadr();
+    int t = 1;
+//    cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
