f6085aac879179eb4df30f55da92936b1a14e565
Yusuf Badr <102367494+yusufbadr@users.noreply.github.com>
2025-02-26 20:57:07 +0200

    Create Tasks and Deadlines.cpp

---
+++ Tasks and Deadlines.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define ll long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define ceil(a, b) ((a+b-1)/b)
+#define mod ((int) 1e9+7)
+#define INF LLONG_MAX
+#define sq(x) ((x)*(x))
+#define sz(a) (int) a.size()
+#define PI (2*acos(0.0)) // M_PI
+
+void YusufBadr(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+//int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+//int dy[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+
+
+void solve(){
+    int n; cin >> n;
+    vector<int> v(n);
+    int sum_d = 0;
+    for(auto&i:v){
+        cin >> i;
+        int d; cin >> d;
+        sum_d+=d;
+    }
+
+    sort(all(v));
+    int curr_time = 0, total_f=0;
+    int sz=(int)v.size();
+
+    for (int i = 0; i < sz; ++i) {
+        curr_time += v[i];
+        total_f += curr_time;
+
+    }
+    cout << sum_d - total_f << endl;
+
+
+
+
+
+}
+
+
+signed main() {
+    YusufBadr();
+    int t = 1;
+//    cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
